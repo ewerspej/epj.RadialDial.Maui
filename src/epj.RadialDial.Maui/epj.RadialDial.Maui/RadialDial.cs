fe7c0981f2ae59f556b82df456e8a071e72c78a2
Julian Ewers <ewerspej@tcd.ie>
2022-09-19 12:23:07 +0200

    started implementation of radial dial

--- src/epj.RadialDial.Maui/epj.RadialDial.Maui/RadialDial.cs
+++ src/epj.RadialDial.Maui/epj.RadialDial.Maui/RadialDial.cs
@@ -6,6 +6,22 @@ namespace epj.RadialDial.Maui;
 
 public class RadialDial : SKCanvasView
 {
+    public int Size { get; set; }
+    public float InternalPadding { get; set; } = 20.0f;
+    public float StartAngle { get; set; } = 0.0f;
+    public float SweepAngle { get; set; } = 320.0f;
+    public float StrokeWidth { get; set; } = 200.0f;
+
+    private SKPoint _center;
+    private SKRect _drawRect;
+    private SKImageInfo _info;
+    private float _adjustedStartAngle;
+
+    public RadialDial()
+    {
+        IgnorePixelScaling = false;
+    }
+
     protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
     {
         base.OnPaintSurface(e);
@@ -13,22 +29,34 @@ public class RadialDial : SKCanvasView
         var canvas = e.Surface.Canvas;
         canvas.Clear();
 
-        var info = e.Info;
-        var smallest = Math.Min(info.Width, info.Height);
+        _info = e.Info;
 
-        var x = info.Width * 0.5f - smallest * 0.5f;
-        var y = info.Height * 0.5f - smallest * 0.5f;
+        Size = Math.Min(_info.Size.Width, _info.Size.Height);
 
-        var arcRect = new SKRect((float)x, (float)y, (float)(x + smallest), (float)(y + smallest));
+        //offsets are used to always center the dial inside the canvas
+        //var horizontalOffset = (_info.Size.Width - Size) / 2;
+        //var verticalOffset = (_info.Size.Height - Size) / 2;
+        var horizontalOffset = StrokeWidth / 2 + InternalPadding;
+        var verticalOffset = StrokeWidth / 2 + InternalPadding;
 
-        canvas.DrawArc(arcRect, 0.0f, 270.0f, true, new SKPaint
+        //setup the rectangle which we will draw in and the center point of the dial
+        _drawRect = new SKRect(horizontalOffset, verticalOffset, Size - horizontalOffset, Size - verticalOffset);
+        _center = new SKPoint(_drawRect.MidX, _drawRect.MidY);
+
+        //the coordinate system of SkiaSharp starts with 0 degrees at 3 o'clock (polar coordinates),
+        //but we want 0 degrees at 0 o'clock, so we rotate everything by -90 degrees.
+        _adjustedStartAngle = StartAngle - 90.0f;
+        
+        using (var path = new SKPath())
         {
-            Style = SKPaintStyle.Fill,
-            Color = Colors.Red.ToSKColor(),
-            StrokeWidth = 5,
-            IsAntialias = true
-        });
-
-        canvas.Translate(arcRect.Width * 0.5f, arcRect.MidY - arcRect.Height * 0.5f);
+            path.AddArc(_drawRect, _adjustedStartAngle, SweepAngle);
+            canvas.DrawPath(path, new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Colors.Red.ToSKColor(),
+                StrokeWidth = StrokeWidth,
+                IsAntialias = true
+            });
+        }
     }
 }
