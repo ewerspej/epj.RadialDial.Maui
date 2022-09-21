8c7a414b6a84d03ed32b6876b1efeae5fc487741
Julian Ewers <ewerspej@tcd.ie>
2022-09-21 11:41:12 +0200

    implemented scale

--- src/epj.RadialDial.Maui/epj.RadialDial.Maui/RadialDial.cs
+++ src/epj.RadialDial.Maui/epj.RadialDial.Maui/RadialDial.cs
@@ -10,8 +10,10 @@ public class RadialDial : SKCanvasView
 
     private int _size;
     private SKCanvas _canvas;
-    private SKPoint _center;
-    private SKRect _drawRect;
+    private SKRect _dialRect;
+    private SKPoint _dialCenter;
+    private SKRect _scaleRect;
+    private SKPoint _scaleCenter;
     private SKImageInfo _info;
     private SKPoint _touchPoint;
     private bool _hasTouch;
@@ -28,14 +30,14 @@ public class RadialDial : SKCanvasView
         set => SetValue(StrokeWidthProperty, value);
     }
 
-    public float Min
+    public int Min
     {
-        get => (float)GetValue(MinProperty);
+        get => (int)GetValue(MinProperty);
         set => SetValue(MinProperty, value);
     }
-    public float Max
+    public int Max
     {
-        get => (float)GetValue(MaxProperty);
+        get => (int)GetValue(MaxProperty);
         set => SetValue(MaxProperty, value);
     }
 
@@ -57,6 +59,36 @@ public class RadialDial : SKCanvasView
         set => SetValue(TouchInputEnabledProperty, value);
     }
 
+    public bool ShowScale
+    {
+        get => (bool)GetValue(ShowScaleProperty);
+        set => SetValue(ShowScaleProperty, value);
+    }
+
+    public int ScaleUnits
+    {
+        get => (int)GetValue(ScaleUnitsProperty);
+        set => SetValue(ScaleUnitsProperty, value);
+    }
+
+    public float ScaleDistance
+    {
+        get => (float)GetValue(ScaleDistanceProperty);
+        set => SetValue(ScaleDistanceProperty, value);
+    }
+
+    public float ScaleLength
+    {
+        get => (float)GetValue(ScaleLengthProperty);
+        set => SetValue(ScaleLengthProperty, value);
+    }
+
+    public float ScaleThickness
+    {
+        get => (float)GetValue(ScaleThicknessProperty);
+        set => SetValue(ScaleThicknessProperty, value);
+    }
+
     public Color DialColor
     {
         get => (Color)GetValue(DialColorProperty);
@@ -69,23 +101,41 @@ public class RadialDial : SKCanvasView
         set => SetValue(BaseColorProperty, value);
     }
 
+    public Color ScaleColor
+    {
+        get => (Color)GetValue(ScaleColorProperty);
+        set => SetValue(ScaleColorProperty, value);
+    }
+
     public static readonly BindableProperty InternalPaddingProperty = BindableProperty.Create(nameof(InternalPadding), typeof(float), typeof(RadialDial), 20.0f, propertyChanged: OnBindablePropertyChanged);
 
     public static readonly BindableProperty StrokeWidthProperty = BindableProperty.Create(nameof(StrokeWidth), typeof(float), typeof(RadialDial), 200.0f, propertyChanged: OnBindablePropertyChanged);
 
-    public static readonly BindableProperty MinProperty = BindableProperty.Create(nameof(Min), typeof(float), typeof(RadialDial), 0.0f);
+    public static readonly BindableProperty MinProperty = BindableProperty.Create(nameof(Min), typeof(int), typeof(RadialDial), 0, propertyChanged: OnBindablePropertyChanged);
 
-    public static readonly BindableProperty MaxProperty = BindableProperty.Create(nameof(Max), typeof(float), typeof(RadialDial), 60.0f);
+    public static readonly BindableProperty MaxProperty = BindableProperty.Create(nameof(Max), typeof(int), typeof(RadialDial), 60, propertyChanged: OnBindablePropertyChanged);
 
     public static readonly BindableProperty ValueProperty = BindableProperty.Create(nameof(Value), typeof(float), typeof(RadialDial), 10.0f, BindingMode.TwoWay, propertyChanged: OnBindablePropertyChanged);
 
     public static readonly BindableProperty SnapToNearestIntegerProperty = BindableProperty.Create(nameof(SnapToNearestInteger), typeof(bool), typeof(RadialDial), true);
 
+    public static readonly BindableProperty TouchInputEnabledProperty = BindableProperty.Create(nameof(TouchInputEnabled), typeof(bool), typeof(RadialDial), false, propertyChanged: OnTouchInputEnabledPropertyChanged);
+
     public static readonly BindableProperty DialColorProperty = BindableProperty.Create(nameof(DialColor), typeof(Color), typeof(RadialDial), Colors.Red, propertyChanged: OnBindablePropertyChanged);
 
     public static readonly BindableProperty BaseColorProperty = BindableProperty.Create(nameof(BaseColor), typeof(Color), typeof(RadialDial), Colors.LightGray, propertyChanged: OnBindablePropertyChanged);
 
-    public static readonly BindableProperty TouchInputEnabledProperty = BindableProperty.Create(nameof(TouchInputEnabled), typeof(bool), typeof(RadialDial), false, propertyChanged: OnTouchInputEnabledPropertyChanged);
+    public static readonly BindableProperty ScaleColorProperty = BindableProperty.Create(nameof(ScaleColor), typeof(Color), typeof(RadialDial), Colors.LightGray, propertyChanged: OnBindablePropertyChanged);
+
+    public static readonly BindableProperty ShowScaleProperty = BindableProperty.Create(nameof(ShowScale), typeof(bool), typeof(RadialDial), true, propertyChanged: OnBindablePropertyChanged);
+
+    public static readonly BindableProperty ScaleUnitsProperty = BindableProperty.Create(nameof(ScaleUnits), typeof(int), typeof(RadialDial), 5, propertyChanged: OnBindablePropertyChanged);
+
+    public static readonly BindableProperty ScaleDistanceProperty = BindableProperty.Create(nameof(ScaleDistance), typeof(float), typeof(RadialDial), 20.0f, propertyChanged: OnBindablePropertyChanged);
+
+    public static readonly BindableProperty ScaleLengthProperty = BindableProperty.Create(nameof(ScaleLength), typeof(float), typeof(RadialDial), 30.0f, propertyChanged: OnBindablePropertyChanged);
+
+    public static readonly BindableProperty ScaleThicknessProperty = BindableProperty.Create(nameof(ScaleThickness), typeof(float), typeof(RadialDial), 10.0f, propertyChanged: OnBindablePropertyChanged);
 
     public RadialDial()
     {
@@ -105,13 +155,76 @@ public class RadialDial : SKCanvasView
         _size = Math.Min(_info.Size.Width, _info.Size.Height);
 
         //offsets are used to always center the dial inside the canvas and move the stroke inwards only
-        var horizontalOffset = StrokeWidth / 2 + InternalPadding;
-        var verticalOffset = StrokeWidth / 2 + InternalPadding;
+        var scaleOffset = InternalPadding;
+        var dialOffset = StrokeWidth / 2 + InternalPadding + ScaleLength + ScaleDistance;
 
-        //setup the rectangle which we will draw in and the center point of the dial
-        _drawRect = new SKRect(horizontalOffset, verticalOffset, _size - horizontalOffset, _size - verticalOffset);
-        _center = new SKPoint(_drawRect.MidX, _drawRect.MidY);
+        //setup the drawing rectangle and center for the scale
+        _scaleRect = new SKRect(scaleOffset, scaleOffset, _size - scaleOffset, _size - scaleOffset);
+        _scaleCenter = new SKPoint(_scaleRect.MidX, _scaleRect.MidY);
 
+        //setup the drawing rectangle and center for the dial
+        _dialRect = new SKRect(dialOffset, dialOffset, _size - dialOffset, _size - dialOffset);
+        _dialCenter = new SKPoint(_dialRect.MidX, _dialRect.MidY);
+
+        DrawScale();
+        DrawBase();
+        DrawDial();
+    }
+
+    private void DrawScale()
+    {
+        if (!ShowScale)
+        {
+            return;
+        }
+
+        //calculate amount and divisor for scale units
+        var scaleDivisor = (Max - Min) / (float)ScaleUnits;
+        var unitCount = (int)Math.Floor(scaleDivisor);
+
+        if ((scaleDivisor - unitCount) * ScaleUnits > 1.0f)
+        {
+            unitCount += 1;
+        }
+
+        //account for scale fractions
+        var clipFactor = unitCount / scaleDivisor;
+        var clippedAngle = 360.0f * clipFactor;
+
+        //calculate angles for scale
+        var angles = new float[unitCount];
+        for (var i = 0; i < unitCount; i++)
+        {
+            angles[i] = clippedAngle / unitCount * i;
+        }
+
+        //draw scale units for each angle
+        foreach (var angle in angles)
+        {
+            var rad = angle.DegreeToRadian();
+
+            var p0 = rad.ToPointOnCircle(_scaleCenter, _scaleRect.Width / 2);
+            var p1 = rad.ToPointOnCircle(_scaleCenter, _scaleRect.Width / 2 - ScaleLength);
+
+            using (var path = new SKPath())
+            {
+                path.AddPoly(new[] { p0, p1 }, close: false);
+                _canvas.Save();
+                _canvas.RotateDegrees(StartAngle, _dialCenter.X, _dialCenter.Y);
+                _canvas.DrawPath(path, new SKPaint
+                {
+                    Style = SKPaintStyle.Stroke,
+                    Color = ScaleColor.ToSKColor(),
+                    StrokeWidth = ScaleThickness,
+                    IsAntialias = true
+                });
+                _canvas.Restore();
+            }
+        }
+    }
+
+    private void DrawDial()
+    {
         float sweepAngle;
         var deltaMaxMin = Max - Min;
 
@@ -119,18 +232,18 @@ public class RadialDial : SKCanvasView
         {
             //calculate the angle of the touch input
             var touchAngle = _touchPoint
-                .MapToCircle(_center, _drawRect.Width / 2)
-                .ToAngle(_center);
+                .MapToCircle(_dialCenter, _dialRect.Width / 2)
+                .ToAngle(_dialCenter);
 
             sweepAngle = (touchAngle + StartAngle).NormalizeAngleTo360();
 
-            var resultValue = deltaMaxMin / 360.0f * sweepAngle;
+            var resultValue = Min + (deltaMaxMin / 360.0f * sweepAngle);
 
             if (SnapToNearestInteger)
             {
                 //round to nearest integer and update sweepAngle
                 var snapValue = (float)Math.Round(resultValue);
-                sweepAngle = 360.0f / deltaMaxMin * snapValue;
+                sweepAngle = 360.0f / deltaMaxMin * (snapValue - Min);
                 Value = snapValue;
             }
             else
@@ -140,24 +253,12 @@ public class RadialDial : SKCanvasView
         }
         else
         {
-            sweepAngle = 360.0f / deltaMaxMin * Value;
-        }
-
-        using (var basePath = new SKPath())
-        {
-            basePath.AddArc(_drawRect, 0, 360);
-            _canvas.DrawPath(basePath, new SKPaint
-            {
-                Style = SKPaintStyle.Stroke,
-                Color = BaseColor.ToSKColor(),
-                StrokeWidth = StrokeWidth,
-                IsAntialias = true
-            });
+            sweepAngle = 360.0f / deltaMaxMin * (Value - Min);
         }
 
         using (var dialPath = new SKPath())
         {
-            dialPath.AddArc(_drawRect, StartAngle, sweepAngle);
+            dialPath.AddArc(_dialRect, StartAngle, sweepAngle);
             _canvas.DrawPath(dialPath, new SKPaint
             {
                 Style = SKPaintStyle.Stroke,
@@ -166,8 +267,21 @@ public class RadialDial : SKCanvasView
                 IsAntialias = true
             });
         }
+    }
 
-        //TODO: draw scale based on Min/Max settings
+    private void DrawBase()
+    {
+        using (var basePath = new SKPath())
+        {
+            basePath.AddArc(_dialRect, 0, 360);
+            _canvas.DrawPath(basePath, new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = BaseColor.ToSKColor(),
+                StrokeWidth = StrokeWidth,
+                IsAntialias = true
+            });
+        }
     }
 
     protected override void OnTouch(SKTouchEventArgs e)
--- src/epj.RadialDial.Maui/epj.RadialDial.Maui/Utilities.cs
+++ src/epj.RadialDial.Maui/epj.RadialDial.Maui/Utilities.cs
@@ -25,7 +25,16 @@ public static class Utilities
         return new SKPoint(center.X + radius * (dX / dist), center.Y + radius * (dY / dist));
     }
 
+    public static SKPoint ToPointOnCircle(this float angle, SKPoint center, float radius)
+    {
+        var x = center.X + radius * (float)Math.Cos(angle);
+        var y = center.Y + radius * (float)Math.Sin(angle);
+        return new SKPoint(x, y);
+    }
+
     public static float RadianToDegree(this float radians) => (float)(180.0 / Math.PI) * radians;
+
+    public static float DegreeToRadian(this float degrees) => degrees / (float) (180.0 / Math.PI);
 
     public static float NormalizeAngleTo360(this float angle) => angle - 360.0f * (float)Math.Floor(angle / 360.0f);
 }
