77b45dab4c79e3ac857b7d75455e5ce53ee33a28
Julian Ewers <ewerspej@tcd.ie>
2022-11-19 17:15:36 +0100

    upgraded to .NET 7.0

--- src/epj.RadialDial.Maui/epj.RadialDial.Maui/Platforms/Tizen/PlatformClass1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace epj.RadialDial.Maui
-{
-    // All the code in this file is only included on Tizen.
-    public class PlatformClass1
-    {
-    }
-}
--- src/epj.RadialDial.Maui/epj.RadialDial.Maui/Platforms/iOS/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace epj.RadialDial.Maui
-{
-    // All the code in this file is only included on iOS.
-    public class PlatformClass1
-    {
-    }
-}
